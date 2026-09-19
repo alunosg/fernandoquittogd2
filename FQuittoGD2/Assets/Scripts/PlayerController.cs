86be0b57909f3041c9804fdd56b6ea34624c2d05
alunosg <alunosg.altodalapa@gmail.com>
2026-09-19 12:27:44 -0300

    bullet
    bullet position

--- FQuittoGD2/Assets/Scripts/PlayerController.cs
+++ FQuittoGD2/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,60 @@
-using UntyEngine;
 using UnityEngine.InputSystem;
 using UnityEngine;
-using JetBrains.Annotations;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Rigidbody rig;
     public Transform cannon;
+    public Transform bulletPoint;
+    public GameObject bulletPrefab;
+    public GameObject shootFX;
 
+    public float bulletSpeed = 10;
     public float speed = 10;
     public Vector2 rotationSpeed = new Vector2(18, 18);
     public float minRotationX = -75;
-    public float minRotationx = 8f;
-    void Start()
+    public float maxRotationX = 8f;
+
+    private Vector2 moveInput;
+    private Vector2 cannonRotation;
+
+    private void FixedUpdate()
     {
-        
+        Vector3 vX = Vector3.zero;
+        Vector3 vY = rig.linearVelocity.y * transform.up;
+        Vector3 vZ = moveInput.y * speed * transform.forward;
+        rig.linearVelocity = vX + vY + vZ;
+
+        float rotation = moveInput.x * Time.fixedDeltaTime * speed * 5;
+        transform.Rotate(0, rotation, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 lookinput = context.ReadValue<Vector2>();
+        cannonRotation.y += lookinput.x + rotationSpeed.y * Time.deltaTime;
+        cannonRotation.x += lookinput.y + rotationSpeed.x * Time.deltaTime;
+
+        cannonRotation.x = Mathf.Clamp(cannonRotation.x, minRotationX, maxRotationX);
+
+        cannon.localRotation = Quaternion.Euler(cannonRotation.x, cannonRotation.y, 0f);
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (shootFX) Instantiate(shootFX, bulletPoint.position, bulletPoint.rotation);
+
+            GameObject bullet = Instantiate(bulletPrefab, bulletPoint.position, bulletPoint.rotation);
+            bullet.GetComponent<Rigidbody>().linearVelocity = bulletPoint.forward * bulletSpeed;
+        }
+
     }
 }
